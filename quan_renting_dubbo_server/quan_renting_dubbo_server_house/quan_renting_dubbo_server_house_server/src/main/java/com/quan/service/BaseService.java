61102fdb9b5b7e81b9658e90b71836fe06fb7ac4
shufu <fsq19981210@gmail.com>
2020-07-22 23:17:53 -0400

    Page Setting

--- quan_renting_dubbo_server/quan_renting_dubbo_server_house/quan_renting_dubbo_server_house_server/src/main/java/com/quan/service/BaseService.java
+++ quan_renting_dubbo_server/quan_renting_dubbo_server_house/quan_renting_dubbo_server_house_server/src/main/java/com/quan/service/BaseService.java
@@ -108,6 +108,7 @@ public abstract class BaseService<T extends BasePojo> {
     public Integer deleteByWhere(T record){
         return this.mapper.delete(new QueryWrapper<>(record));
     }
+
     public IPage<T> queryPageList(QueryWrapper<T> queryWrapper, Integer page,
                                   Integer rows) {
 // 获取分页数据
